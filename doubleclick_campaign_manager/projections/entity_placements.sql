5b04617c8f182c82ce16e112598c9a746a100689
maxcobert <max@panoramicHQ.com>
2020-11-19 12:55:38 -0800

    second attempt DCM advertiser_id mapping

--- doubleclick_campaign_manager/projections/entity_placements.sql
+++ doubleclick_campaign_manager/projections/entity_placements.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE VIEW "DOUBLECLICK_CAMPAIGN_MANAGER_VIEWS"."ENTITY_PLACEMENTS" AS
 SELECT ACCOUNT_ID::VARCHAR                                                  AS ACCOUNT_ID
+       , ADVERTISER_ID::VARCHAR                                             AS ADVERTISER_ID
        , CAMPAIGN_ID::VARCHAR                                               AS CAMPAIGN_ID
        , SITE_ID_DCM_::VARCHAR                                              AS SITE_ID
        , SITE_DCM_::VARCHAR                                                 AS SITE
@@ -25,6 +26,7 @@ SELECT ACCOUNT_ID::VARCHAR                                                  AS A
 FROM (SELECT *,
              ROW_NUMBER() OVER (PARTITION BY
                ACCOUNT_ID,
+               ADVERTISER_ID,
                PLACEMENT_ID
                ORDER BY _FIVETRAN_SYNCED DESC) ___R
       FROM "DOUBLE_CLICK_CAMPAIGN_MANAGER"."GENERAL_METRICS_MAIN_ACCOUNT"
